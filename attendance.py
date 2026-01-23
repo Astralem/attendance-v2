271594d486c4b397baf95fa27f670f291ddbf104
Crolloh <latrellbang@gmail.com>
2026-01-23 16:06:34 +0800

    added grade 11 ceferino table with all other functions, combined all of the students into students but they will be sorted out by grade level

--- attendance.py
+++ attendance.py
@@ -19,12 +19,22 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS students (
             student_id INTEGER PRIMARY KEY,
-            student_names TEXT
+            student_names TEXT NOT NULL,
+            grade_level INTEGER NOT NULL
+        )
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS attendance_11 (
+            student_id INTEGER PRIMARY KEY,
+            student_names TEXT,
+            time_in TEXT DEFAULT NULL,
+            status TEXT DEFAULT 'N/A',
+            FOREIGN KEY (student_id) REFERENCES students(student_id)
         )
     """)
 
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS attendance (
+        CREATE TABLE IF NOT EXISTS attendance_12 (
             student_id INTEGER PRIMARY KEY,
             student_names TEXT,
             time_in TEXT DEFAULT NULL,
@@ -36,29 +46,39 @@ def init_db():
     conn.commit()
     conn.close()
 
-def add_student(student_id, student_name):
+def get_gradelevel(grade_level):
+    if grade_level == 11:
+        return "attendance_11"
+    elif grade_level == 12:
+        return "attendance_12"
+    else: 
+        raise ValueError("Invalid grade level")
+    
+def add_student(student_id, student_name, grade_level):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
-        INSERT OR IGNORE INTO students (student_id, student_names)
-        VALUES (?, ?)
-    """, (student_id, student_name))
+        INSERT OR IGNORE INTO students (student_id, student_names, grade_level)
+        VALUES (?, ?, ?)
+    """, (student_id, student_name, grade_level))
 
-    cursor.execute("""
-        INSERT OR IGNORE INTO attendance (student_id, student_names)
+    gradeLevel = get_gradelevel(grade_level)
+
+    cursor.execute(f"""
+        INSERT OR IGNORE INTO {gradeLevel} (student_id, student_names)
         VALUES (?, ?)
     """, (student_id, student_name))
 
     conn.commit()
     conn.close()
 
-def record_attendance(student_id):
+def record_attendance(student_id, grade_level):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute(
-        "SELECT student_names FROM students WHERE student_id = ?",
+        "SELECT student_names, grade_level FROM students WHERE student_id = ?",
         (student_id,)
     )
     student = cursor.fetchone()
@@ -66,9 +86,18 @@ def record_attendance(student_id):
     if not student:
         conn.close()
         return "Unregistered ID"
+    
+    name = student[0]
+    gradeLevel = student[1]
+
+    if gradeLevel != grade_level:
+        conn.close()
+        return f"{name} is not in {gradeLevel}"
+    
+    table = get_gradelevel(grade_level)
 
     cursor.execute(
-        "SELECT time_in FROM attendance WHERE student_id = ?",
+        f"SELECT time_in FROM {table} WHERE student_id = ?",
         (student_id,)
     )
     already_present = cursor.fetchone()
@@ -79,8 +108,8 @@ def record_attendance(student_id):
 
     time_in = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    cursor.execute("""
-        UPDATE attendance
+    cursor.execute(f"""
+        UPDATE {table}
         SET time_in = ?, status = ?
         WHERE student_id = ?
     """, (time_in, "Present", student_id))
@@ -90,11 +119,12 @@ def record_attendance(student_id):
 
     return f"{student[0]} marked present"
 
-def remove_student(student_id):
+def remove_student(student_id, grade_level):
     conn = get_connection()
     cursor = conn.cursor()
     #DELETE FROM ATTENDANCE
-    cursor.execute("DELETE FROM attendance WHERE student_id = ?", 
+    table = get_gradelevel(grade_level)
+    cursor.execute(f"DELETE FROM {table} WHERE student_id = ?", 
     (student_id,)
     )
     #DELETE FROM STUDENTS
@@ -105,12 +135,13 @@ def remove_student(student_id):
     conn.commit()
     conn.close()
     
-def clear_attendance():
+def clear_attendance(grade_level):
     conn = get_connection()
     cursor = conn.cursor()
     
-    cursor.execute("""
-        UPDATE attendance
+    table = get_gradelevel(grade_level)
+    cursor.execute(f"""
+        UPDATE {table}
         SET time_in = NULL , status = NULL
     """)
 
